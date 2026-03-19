b6032b876257d312b550767bca8838e85a4d6c55
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-03-19 11:28:46 -0300

    FEAT: Migração para o novo projeto com SimpleJWT

---
+++ core/models/autor.py
@@ -0,0 +1,17 @@
+from tabnanny import verbose
+
+from django.db import models
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, null=True)
+
+    class Meta:
+
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+
+    def __str__(self):
+        return f"({self.id}) {self.nome} "
+
