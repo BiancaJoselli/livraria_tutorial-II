30acdd0550d9303e087dd3f8bbfe978cdae29c3c
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-08-14 10:07:18 -0300

    FEAT: organizando os dados dos itens que a api entregará para o front

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,24 +1,23 @@
+from django.forms.fields import DecimalField
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, DecimalField, ModelSerializer, SerializerMethodField
 from core.models import Compra
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
-    total = SerializerMethodField()
-
-    def get_total(self, instance):
-        return instance.livro.preco * instance.quantidade
+    titulo =CharField(source='livro.titulo', read_only=True)
+    editora = CharField(source='livro.editora.nome', read_only=True)
+    preco = DecimalField(
+        source='livro.preco', 
+        max_digits=7, 
+        decimal_places=2, 
+        read_only=True)
+    
+    capa = CharField(source='livro.capa.url', read_only=True)
     
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade', 'total')
-        depth = 1
-
-class ItensCompraSerializer(ModelSerializer):
-    class Meta:
-        model = ItensCompra
-        fields = ('livro', 'quantidade')
-
+        fields = ('id', 'titulo', 'editora', 'quantidade', 'preco', 'capa')
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True) # inclua essa linha 
