9aa00a4d4d9def8359a99fc8483a1f12e6e44833
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-09-04 10:29:17 -0300

    FEAT: criação de uma compra a partir do usuário autenticado

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,6 +1,6 @@
 from django.forms.fields import DecimalField
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import CharField, DecimalField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, CurrentUserDefault, HiddenField, DecimalField, ModelSerializer, SerializerMethodField
 from core.models import Compra
 from core.models import Compra, ItensCompra
 from django.db import transaction
@@ -27,7 +27,7 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
         fields = ('livro', 'quantidade')
 
 class CompraCreateUpdateSerializer(ModelSerializer):
-    itens = ItensCompraCreateUpdateSerializer(many=True)
+    usuario = HiddenField(default=CurrentUserDefault())
 
     class Meta:
         model = Compra
@@ -61,6 +61,14 @@ class CompraSerializer(ModelSerializer):
         model = Compra
         fields = ('id', 'usuario', 'status', 'total', 'itens')
 
+class ItensCompraListSerializer(ModelSerializer):
+    livro = CharField(source='livro.titulo', read_only=True)
+
+    class Meta:
+        model = ItensCompra
+        fields = ('quantidade', 'livro')
+        depth = 1
+
 class CompraListSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     itens = ItensCompraListSerializer(many=True, read_only=True)
@@ -69,10 +77,3 @@ class CompraListSerializer(ModelSerializer):
         model = Compra
         fields = ('id', 'usuario', 'itens')        
 
-class ItensCompraListSerializer(ModelSerializer):
-    livro = CharField(source='livro.titulo', read_only=True)
-
-    class Meta:
-        model = ItensCompra
-        fields = ('quantidade', 'livro')
-        depth = 1
