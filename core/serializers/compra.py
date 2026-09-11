c49bea6c96101d6627fde86ebc08826ce9b72a1c
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-09-11 10:52:12 -0300

    FIX: Excluindo preço do livro e adicionando basename na urls do compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -9,12 +9,6 @@ from django.db import transaction
 class ItensCompraSerializer(ModelSerializer):
     titulo =CharField(source='livro.titulo', read_only=True)
     editora = CharField(source='livro.editora.nome', read_only=True)
-    preco = DecimalField(
-        source='livro.preco', 
-        max_digits=7, 
-        decimal_places=2, 
-        read_only=True)
-    
     capa = CharField(source='livro.capa.url', read_only=True)
     
     class Meta:
