f556233d408a2232d9c9a4facbcfa531b158a63c
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-09-18 10:36:10 -0300

    FEAT: Alterando o preço de um livro

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,8 +1,11 @@
+from rest_framework import status
 from core.views.user import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema
 
 from core.models import Livro
-from core.serializers import LivroRetrieveSerializer, LivroListSerializer, LivroSerializer
+from core.serializers import LivroAlterarPrecoSerializer, LivroRetrieveSerializer, LivroListSerializer, LivroSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.order_by('titulo')
@@ -14,3 +17,26 @@ class LivroViewSet(ModelViewSet):
         elif self.action == 'retrieve':
             return LivroRetrieveSerializer
         return LivroSerializer
+    
+    @extend_schema(
+            request=LivroAlterarPrecoSerializer,
+            responses={200: None},
+            description="Altera o preço de um livro específico.",
+            summary="Alterar preço do livro",
+        )
+
+    @action(detail=True, methods=['patch'])
+    def alterar_preco(self, request, pk=None):
+        livro = self.get_object()
+
+        serializer = LivroAlterarPrecoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        livro.preco = serializer.validated_data['preco']
+        livro.save()
+
+        return Response(
+            {'detail': f'Preço do livro "{livro.titulo}" atualizado para {livro.preco}.'}, status=status.HTTP_200_OK
+        ) 
+    
+    
